d1cc61872cf225affb703d5076038cfd0186c5d5
v.florio <floriovincenzo98@gmail.com>
2025-12-18 20:00:50 +0100

    apps workspace

---
+++ apps/demo-ui/src/main.tsx
@@ -0,0 +1,17 @@
+import { DEFAULT_EXTENSION_STATE } from "@hbb-emu/extension-common";
+import { Settings } from "@hbb-emu/settings-ui";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Settings
+      sideEffects={{
+        load: async () => DEFAULT_EXTENSION_STATE,
+        save: async () => {},
+        playChannel: async () => {},
+        subscribe: () => () => {},
+      }}
+    />
+  </StrictMode>,
+);
